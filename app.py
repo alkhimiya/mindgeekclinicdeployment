526ea6884f70edead72f273012b09fb10e60e8c5
alkhimiya <121459089+alkhimiya@users.noreply.github.com>
2026-01-17 21:47:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ elif menu == "🩺 Consulta Médica Gratis":
                     st.markdown(res)
                     st.session_state.messages.append({"role": "assistant", "content": res})
                     
-                    if "CLAVE_ORDEN:" in res:
+         if "CLAVE_ORDEN:" in res:
     st.session_state.diagnostico_nexo = res.split("CLAVE_ORDEN:")[-1].strip()
     st.session_state.orden_lista = True
     st.success("✅ Protocolo de Decodificación Finalizado. Diríjase al Área Administrativa.")
